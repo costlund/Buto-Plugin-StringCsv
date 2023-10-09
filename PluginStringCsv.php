7137068ff8505ed101437b5450e792f014691555
Claes Östlund <claes@stenbergit.se>
2023-10-09 17:06:19 +0200

    1.1.0

--- PluginStringCsv.php
+++ PluginStringCsv.php
@@ -2,17 +2,22 @@
 class PluginStringCsv{
   /**
    * Convert a csv string to array.
-   * Separated by ";".
+   * Separated by ";" or tab.
    * @param string $str String in csv format.
+   * @param string $delimiter semicolon (default) or tab.
    * @return array
    */
-  public function stringToArray($str){
+  public function stringToArray($str, $delimiter = 'semicolon'){
     wfPlugin::includeonce('string/array');
     $sa = new PluginStringArray();
     $rows = $sa->from_br($str);
     $data = array();
     foreach($rows as $v){
-      $data[] = $sa->from_char($v, ';');
+      if($delimiter=='semicolon'){
+        $data[] = $sa->from_char($v, ';');
+      }elseif($delimiter=='tab'){
+        $data[] = $sa->from_tab($v);
+      }
     }
     return $data;
   }
--- README.md
+++ README.md
@@ -7,3 +7,10 @@ wfPlugin::includeonce('string/csv');
 $csv = new PluginStringCsv();
 $content = $csv->stringToArray($content);
 ```
+
+### Delimiter
+Default delimiter is semicolon (;).
+To convert csv data with tabs add tab as second parameter.
+```
+$content = $csv->stringToArray($content, 'tab');
+```
